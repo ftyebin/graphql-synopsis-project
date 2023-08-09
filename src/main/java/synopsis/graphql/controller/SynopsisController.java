9fdd7596cc09702bc950b424c7e1398bd432f41f
ftyebin <i449@sk.com>
2023-08-09 10:50:02 +0900

    [feat]: Synopsis Controller  - EUXP, SMD, SCS Response Data 통합하여 GraphQL로 Response

---
+++ src/main/java/synopsis/graphql/controller/SynopsisController.java
@@ -0,0 +1,38 @@
+package synopsis.graphql.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+import synopsis.graphql.model.dto.request.RequestData;
+import synopsis.graphql.model.dto.request.RequestEuxpData;
+import synopsis.graphql.model.dto.request.RequestScsData;
+import synopsis.graphql.model.dto.request.RequestSmdData;
+import synopsis.graphql.model.dto.response.SynopsisData;
+import synopsis.graphql.service.EuxpService;
+import synopsis.graphql.service.ScsService;
+import synopsis.graphql.service.SmdService;
+
+
+@Controller
+@RequiredArgsConstructor
+public class SynopsisController {
+
+    private final EuxpService euxpService;
+    private final SmdService smdService;
+    private final ScsService scsService;
+
+    @QueryMapping
+    public SynopsisData synopsisPage(@Argument RequestData requestData) {
+
+        RequestEuxpData requestEuxpData = new RequestEuxpData(requestData);
+        RequestSmdData requestSmdData = new RequestSmdData(requestData);
+        RequestScsData requestScsData = new RequestScsData(requestData);
+
+        return SynopsisData.builder()
+                .euxpResult(euxpService.getEuxpResult(requestEuxpData))
+                .smdResult(smdService.getSmdResult(requestSmdData))
+                .scsResult(scsService.getScsResult(requestScsData))
+            .build();
+    }
+}
