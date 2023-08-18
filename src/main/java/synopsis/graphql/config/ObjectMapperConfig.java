8a7b4932031b402723445e4d0b8bc0ac12b89c6e
ftyebin <i449@sk.com>
2023-08-18 14:23:47 +0900

    [add]: ObjectMapper 설정 추가 - field 없는 경우 해결

--- src/main/java/synopsis/graphql/config/ObjectMapperConfig.java
+++ src/main/java/synopsis/graphql/config/ObjectMapperConfig.java
@@ -1,5 +1,7 @@
 package synopsis.graphql.config;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +11,9 @@ public class ObjectMapperConfig {
 
     @Bean
     public ObjectMapper objectMapper() {
-        return new ObjectMapper();
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES,false);
+        return objectMapper;
     }
 }
