48886643fd66a736f6468e99dcfa9d2dc2d9b82b
ftyebin <i449@sk.com>
2023-08-18 10:43:39 +0900

    [refactor]: GraphqlExceptionResolver 리팩토링

---
+++ src/main/java/synopsis/graphql/excpetion/GraphqlExceptionResolver.java
@@ -0,0 +1,35 @@
+package synopsis.graphql.excpetion;
+
+import graphql.ErrorType;
+import graphql.GraphQLError;
+import graphql.GraphqlErrorBuilder;
+import graphql.schema.DataFetchingEnvironment;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.graphql.execution.DataFetcherExceptionResolverAdapter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GraphqlExceptionResolver extends DataFetcherExceptionResolverAdapter {
+
+    @Override
+    protected GraphQLError resolveToSingleError(@NotNull Throwable ex, @NotNull DataFetchingEnvironment env) {
+        ErrorType errorType;
+
+        if (ex instanceof ServerRequestException) {
+            errorType = ErrorType.ExecutionAborted;
+        } else if (ex instanceof JsonToObjectException || ex instanceof JsonPropertyException) {
+            errorType = ErrorType.ValidationError;
+        } else if (ex instanceof ResultDataNotFoundException) {
+            errorType = ErrorType.DataFetchingException;
+        } else {
+            return null;
+        }
+
+        return GraphqlErrorBuilder.newError()
+                .errorType(errorType)
+                .message(ex.getMessage())
+                .path(env.getExecutionStepInfo().getPath())
+                .location(env.getField().getSourceLocation())
+                .build();
+    }
+}
