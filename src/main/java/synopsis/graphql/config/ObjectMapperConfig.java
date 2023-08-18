2a325ebd791e6ebf982bbe54e69c69a64820d6ee
ftyebin <i449@sk.com>
2023-08-18 11:28:26 +0900

    [add]: Custom Exception - JsonConversionException 추가

--- src/main/java/synopsis/graphql/config/ObjectMapperConfig.java
+++ src/main/java/synopsis/graphql/config/ObjectMapperConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ObjectMapperConfig {
+
     @Bean
     public ObjectMapper objectMapper() {
         return new ObjectMapper();
--- src/main/java/synopsis/graphql/util/converter/AbstractJsonToObjectConverter.java
+++ src/main/java/synopsis/graphql/util/converter/AbstractJsonToObjectConverter.java
@@ -1,23 +1,23 @@
 package synopsis.graphql.util.converter;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.exc.UnrecognizedPropertyException;
 import lombok.extern.slf4j.Slf4j;
 import synopsis.graphql.config.ObjectMapperHolder;
+import synopsis.graphql.excpetion.JsonConversionException;
 import synopsis.graphql.excpetion.JsonPropertyException;
 import synopsis.graphql.excpetion.JsonToObjectException;
 
 import java.util.Optional;
 
 @Slf4j
-public class AbstractJsonToObjectConverter<T> implements  JsonToObjectConverter<T> {
+public abstract class AbstractJsonToObjectConverter<T> implements  JsonToObjectConverter<T> {
 
     private final Class<T> type;
     private final String typeName;
 
-    public AbstractJsonToObjectConverter(Class<T> type, String typeName) {
+    protected AbstractJsonToObjectConverter(Class<T> type, String typeName) {
         this.type = type;
         this.typeName = typeName;
     }
@@ -27,14 +27,19 @@ public class AbstractJsonToObjectConverter<T> implements  JsonToObjectConverter<
 
         ObjectMapper objectMapper = ObjectMapperHolder.getMapper();
 
+        String errorMessage;
+
         try {
             return Optional.ofNullable(objectMapper.readValue(jsonData, type));
         } catch (UnrecognizedPropertyException e) {
-            throw new JsonPropertyException(typeName + " result에서 예상치 못한 json field 발견 " + e.getMessage());
-        } catch (JsonMappingException e) {
-            throw new RuntimeException(e);
+            errorMessage = String.format("%s result 에서 예상치 못한 json field 발견 : %s", typeName, e.getMessage());
+            throw new JsonPropertyException(errorMessage);
         } catch (JsonProcessingException e) {
             throw new JsonToObjectException("Error found at " + e.getLocation() + " | " + typeName + " Json Object Mapping Error : " + e.getMessage());
+        } catch (Exception e) {
+            errorMessage = String.format("%s 에서 Json 데이터 변환 실패 : %s", typeName, e);
+            log.error(errorMessage);
+            throw new JsonConversionException(errorMessage);
         }
     }
 }
