c094c4fe04bc8f0f65fe0a68ad1711a64f607dc4
ftyebin <i449@sk.com>
2023-08-19 18:30:02 +0900

    [add]: ResttemplateBuilder - HTTP 요청 및 데이터 읽기 시간 옵션 추가

--- src/main/java/synopsis/graphql/config/RestTemplateConfig.java
+++ src/main/java/synopsis/graphql/config/RestTemplateConfig.java
@@ -3,10 +3,11 @@ package synopsis.graphql.config;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.DefaultUriBuilderFactory;
+import java.time.Duration;
 
 @Component
 @Getter
@@ -14,14 +15,12 @@ import org.springframework.web.util.DefaultUriBuilderFactory;
 @RequiredArgsConstructor
 public class RestTemplateConfig {
 
+    private static final int DURATION_TIME_MILLIS = 500;
     @Bean
-    public RestTemplate restTemplate() {
-        DefaultUriBuilderFactory defaultUriBuilderFactory = new DefaultUriBuilderFactory();
-        defaultUriBuilderFactory.setEncodingMode(DefaultUriBuilderFactory.EncodingMode.NONE);
-
-        RestTemplate restTemplate = new RestTemplate();
-        restTemplate.setUriTemplateHandler(defaultUriBuilderFactory);
-
-        return restTemplate;
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder
+                .setConnectTimeout(Duration.ofMillis(DURATION_TIME_MILLIS))
+                .setReadTimeout(Duration.ofMillis(DURATION_TIME_MILLIS))
+                .build();
     }
 }
