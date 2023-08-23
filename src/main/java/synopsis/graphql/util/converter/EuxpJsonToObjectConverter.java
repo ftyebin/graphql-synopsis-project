c7c01c21c0c935e205fbf0ffc8227d45712d702d
ftyebin <139842905+ftyebin@users.noreply.github.com>
2023-08-23 10:59:05 +0000

    [fix]: 추상화에 따른 EuxpJsonCoverter 로직 간소화

--- src/main/java/synopsis/graphql/util/converter/EuxpJsonToObjectConverter.java
+++ src/main/java/synopsis/graphql/util/converter/EuxpJsonToObjectConverter.java
@@ -1,31 +1,14 @@
 package synopsis.graphql.util.converter;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 import synopsis.graphql.model.euxp.EuxpResult;
 
-@Slf4j
-public
-class EuxpJsonToObjectConverter {
+@Service
+public class EuxpJsonToObjectConverter extends AbstractJsonToObjectConverter<EuxpResult> {
 
-    private EuxpJsonToObjectConverter(){
-        throw new IllegalStateException("Euxp Json to Object Converter");
+    public EuxpJsonToObjectConverter(ObjectMapper objectMapper) {
+        super(EuxpResult.class, "EUXP", objectMapper);
     }
 
-    public static EuxpResult convert(String jsonData) throws RuntimeException {
-        ObjectMapper objectMapper = new ObjectMapper();
-
-        try {
-
-            return objectMapper.readValue(jsonData, EuxpResult.class);
-
-        } catch (JsonMappingException e) {
-            e.printStackTrace();
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
-        return null;
-    }
 }
